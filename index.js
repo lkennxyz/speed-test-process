8230d5b3d4eff4d7fafa958e085d5b1ecd66a7ab
lkennxyz <liam.kennedy1408@gmail.com>
2019-02-19 19:16:06 +0000

    First upload with fast writing to console

---
+++ index.js
@@ -0,0 +1,13 @@
+// const speedTest = require('speed-test');
+import getSpeed from 'fast-speed-test';
+
+async function fast() {
+  const Bps = await getSpeed(10);
+  return Bps/125000;
+}
+
+(async () => {
+  console.log(await fast() + ' mbps');
+  process.exit(0);
+})();
+
